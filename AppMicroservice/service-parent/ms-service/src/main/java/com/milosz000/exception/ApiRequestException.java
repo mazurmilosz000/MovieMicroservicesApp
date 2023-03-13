dac9af02e59c31bbed34caec4b9db476a3f41173
Milosz Mazur <mazur.milosz000@gmail.com>
2023-03-13 22:00:33 +0100

    added ms-service

---
+++ AppMicroservice/service-parent/ms-service/src/main/java/com/milosz000/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.milosz000.exception;
+
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
