f54673e3cc6b1ce8366f2149468ee35ecaccec21
Milosz Mazur <mazur.milosz000@gmail.com>
2023-04-04 22:06:42 +0200

    added api-gateway/src/

---
+++ AppMicroservice/service-parent/api-gateway/src/main/java/com/milosz000/ApiGateway.java
@@ -0,0 +1,12 @@
+package com.milosz000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiGateway.class, args);
+    }
+}
